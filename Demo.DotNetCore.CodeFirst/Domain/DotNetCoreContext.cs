c480da18bf9ed1abff5127bcb9c5956c1152d6a5
Test <noel.leclerc@web4cloud.com>
2017-09-09 11:36:33 +0200

    Ajoutez des fichiers projet.

---
+++ Demo.DotNetCore.CodeFirst/Domain/DotNetCoreContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.DotNetCore.CodeFirst.Domain
+{
+    public class DotNetCoreContext : DbContext
+    {
+
+        public DotNetCoreContext()
+        {
+
+        }
+
+        public DotNetCoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> User { get; set; }
+
+    }
+}
